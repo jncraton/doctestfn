a76c51174638867941b0e0386cd5457218af327f
Jon Craton <jncraton@gmail.com>
2021-09-30 10:47:06 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="doctestfn",
+    version="1.0.0",
+    author="Jon Craton",
+    author_email="jon@joncraton.com",
+    description="Run doctests for a single function",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jncraton/doctestfn",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'doctestfn=doctestfn:main',
+        ],
+    },
+    install_requires=[
+    ],
+)
